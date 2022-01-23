a57a5a8a9e4d486f1ff991a4390d368c2b26b464
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-01-23 16:52:44 -0600

    javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+new Date()
+
+
 $(document).ready(function (){
     $(".saveBtn").on("click",function(){
         hour-9 //put description, 2 variables 1 for time, 1 for value(value is description)
+        
     })
 })
