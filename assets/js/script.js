7f5f68f0bc6007502267a3d876dba1a9ab6f1f89
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-01-23 23:50:21 -0600

    date and time variables added to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-new Date()
+var currentDay = moment().format("dddd LL");
+$("#currentDay").append(currentDay);
 
+var currentTime = moment().format("LT");
+$("#currentTime").append(currentTime);
 
 $(document).ready(function (){
     $(".saveBtn").on("click",function(){
-        var value = $(this).siblings(".description").val() //put description, 2 variables 1 for time, 1 for value(value is description)
+        var value = $(this).siblings(".description").val() 
         var time = $(this).parent().attr("id")
         localStorage.setItem(time,value)
     })
@@ -19,15 +22,25 @@ $(document).ready(function (){
                 $(this).removeClass("past")
             }
             else {
+                $(this).addClass("future")
+                $(this).removeClass("present")
 
             }
         })
     }
     hourUpdater();
     $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+    
 });
 
 
 
 
-//color change??? function to return that value?? $(.past) $(.present) $(.future)
